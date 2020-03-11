9148bb262259421471a25aea56ad35578a9887ad
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-03-11 21:41:22 +1030

    fixed timer

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 //timer//
-var timer = document.querySelector("time");
+var timer = document.querySelector("#time");
 
 var secondsLeft = 60;
 
 function setTime() {
-timerInterval =setInterval(function() {
+var timerInterval =setInterval(function() {
 secondsLeft--;
-timer.innerText = secondsLeft + "time left";
+timer.textContent = secondsLeft + "time left";
 console.log(secondsLeft)
-document.querySelector("time").innerHTML = secondsLeft;
 
-//When Timer Runs Out//
+
 if (secondsLeft === 0) {
     clearInterval(timerInterval)
     sendMessage ();
 }
+console.log(secondsLeft)
 
 }, 1000);
 }
 
 function sendMessage() {
-    timer.textContent = "TIME IS UP";
+    timer.textContent = "Your Time is Up";
 }
 
 setTime ();
