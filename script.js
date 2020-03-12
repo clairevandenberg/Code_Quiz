478113dd86b00ee7934579e42f1c269e09fca011
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-03-12 15:13:05 +1030

    added questions to html page with buttons for answers

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//timer//
+//setting the timer//
+
 var timer = document.querySelector("#time");
 
 var secondsLeft = 60;
@@ -9,7 +10,7 @@ secondsLeft--;
 timer.textContent = secondsLeft + "time left";
 console.log(secondsLeft)
 
-
+//when timer ends//
 if (secondsLeft === 0) {
     clearInterval(timerInterval)
     sendMessage ();
@@ -18,7 +19,7 @@ console.log(secondsLeft)
 
 }, 1000);
 }
-
+//timer has ended message//
 function sendMessage() {
     timer.textContent = "Your Time is Up";
 }
